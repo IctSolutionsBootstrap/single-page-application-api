9de972426df967be3df60f2fb5e4b2ab7ef7ee28
unknown <bernabasjosef@gmail.com>
2023-11-30 20:20:59 +0300

    Add render.yaml

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_25_173018) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "input_data", force: :cascade do |t|
